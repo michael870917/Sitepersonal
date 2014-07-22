d91b97afc00bd729213527c943b52a6abaee595e
michael870917 <michael870917@gmail.com>
2014-07-22 14:48:20 -0500

    carga inicial de archivos
    carga inicial de archivos

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<title>Ing. Edwin Michael</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">	
+	<link href="css/main.css" rel="stylesheet" type="text/css" >
+	<link href="css/normalize.css" rel="stylesheet" type="text/css" >	
+	<!--<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/actionNav.js" type="text/javascript"></script>-->
+</head>
+
+<body>
+<header class="header">
+	<div class="logo">
+		<img src="images/logo.png" alt="Puls4">		
+	</div>
+	<div class="titular">
+		<h1 class="titulo">
+			Puls4: Comunidad profesional de gente atractiva
+		</h1>
+		<div>
+			<a class="filtro" href="#">
+				Stylus
+			</a>
+			<a class="publicar" href="#">
+				Publicar
+			</a>				
+		</div>
+	</div>
+	<div class="usuario">
+		<figure class="avatar">
+			<img src="images/avatar.jpg" alt="Michael">
+		</figure>
+		<a class="flechita" href="#"></a>
+	</div>
+</header>
+<!--
+<nav class="clearfix">
+	<ul class="clearfix">
+		<li><a href="#">Quien soy</a></li>
+		<li><a href="#">Servicios</a></li>
+		<li><a href="#">Desarrollos</a></li>
+		<li><a href="#">Contacto</a></li>
+	</ul>
+	<a href="#" id="pull">Menu</a>
+</nav>
+-->
+</body>
+</html>
+
